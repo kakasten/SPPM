d3e59588aecf8c844551c1e2645fe40d4ab8c88b
Luiz Fernando <kakasten.cipher@gmail.com>
2024-06-18 18:49:33 -0300

    Adding system logic

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,13 @@ const XTAL_FREQ_HZ: u32 = 12_000_000u32;
 
 #[rp2040_hal::entry]
 fn main() -> ! {
-    // Grab our singleton objects
-    let mut pac = pac::Peripherals::take().unwrap();
-    let _core = pac::CorePeripherals::take().unwrap();
 
-    // Set up the watchdog driver - needed by the clock setup code
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
-    // Configure the clocks
-    let _clocks = hal::clocks::init_clocks_and_plls(
+    let clocks = hal::clocks::init_clocks_and_plls(
         XTAL_FREQ_HZ,
         pac.XOSC,
         pac.CLOCKS,
@@ -38,6 +36,8 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
     let sio = hal::Sio::new(pac.SIO);
     let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
@@ -46,7 +46,27 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
+    let button_one_pin = pins.gpio16.into_pull_up_input();
+    let button_two_pin = pins.gpio17.into_pull_up_input();
+    let sensor_pin = pins.gpio18.into_pull_up_input();
+
+    let mut close_door_pin = pins.gpio21.into_push_pull_output();
+    let mut close_valve_pin = pins.gpio22.into_push_pull_output();
+
     loop {
-        
+        if button_one_pin.is_low().unwrap() && button_two_pin.is_low().unwrap(){
+            close_door_pin.set_high().unwrap();
+            delay.delay_ms(1500);
+            loop {
+                if sensor_pin.is_high().unwrap(){
+                    close_valve_pin.set_high().unwrap();
+                    delay.delay_ms(2000);
+                    close_valve_pin.set_low().unwrap();
+                    delay.delay_ms(500);
+                    close_door_pin.set_low().unwrap();
+                    break;
+                }
+            }
+        }
     }
 }
